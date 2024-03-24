9786f8188a96aa9681b765ec82a50b3369aac4ab
Onwordi Prince Alfred <princelini4@gmail.com>
2024-03-24 18:20:07 +0100

    type update

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,20 @@
 export type Questions = {
-  title: string;
+  id: string;
+  question: string;
+  options: Array<string>;
+};
+
+export type Question = {
+  question: string;
   options: Array<string>;
 };
 
 export type Token = {
   token: string;
+};
+
+export type Action = {
+  isLoading: boolean;
+  error: string;
+  action?: () => void;
 };
