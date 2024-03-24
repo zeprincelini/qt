1ef8ce89e5c6743f754eb6ec84337d77e91ad1b2
Onwordi Prince Alfred <princelini4@gmail.com>
2024-03-24 18:15:48 +0100

    added header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="py-3 px-1 w-full bg-blue-500">
+      <p className="text-white text-[18px]">Question Time</p>
+    </div>
+  );
+};
+
+export default Header;
