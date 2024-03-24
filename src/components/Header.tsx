5fa355525419c1668a68993d4aedd3dae5c823fc
Onwordi Prince Alfred <princelini4@gmail.com>
2024-03-24 22:39:37 +0100

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,17 @@
+"use client";
+
 import React from "react";
+import { redirect } from "next/navigation";
 
 const Header = () => {
   return (
-    <div className="py-3 px-1 w-full bg-blue-500">
-      <p className="text-white text-[18px]">Question Time</p>
+    <div className="py-3 px-2 w-full bg-blue-500">
+      <p
+        className="text-white text-[18px] cursor-pointer"
+        onClick={() => redirect("/")}
+      >
+        Question Time
+      </p>
     </div>
   );
 };
