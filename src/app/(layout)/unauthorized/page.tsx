0da42a6cc0d5c6f2535cd44560b13b33ae878075
Onwordi Prince Alfred <princelini4@gmail.com>
2024-03-24 22:48:03 +0100

    cleanup

--- src/app/(layout)/unauthorized/page.tsx
+++ src/app/(layout)/unauthorized/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+import { useEffect } from "react";
 import { redirect } from "next/navigation";
 import { useAction } from "../../../hooks/useAction";
 import Button from "../../../components/shared/Button";
+import ErrorText from "../../../components/ErrorText";
 import { generateToken, getToken } from "../../../server/api";
-import { useEffect } from "react";
 
 const Unauthorized = () => {
   const toHome = () => redirect("/");
@@ -20,9 +21,7 @@ const Unauthorized = () => {
     <div className="grid place-content-center min-h-[calc(100vh-150px)]">
       <div className="flex flex-col gap-2 text-center">
         <p>Token not found!</p>
-        {error ? (
-          <p className="text-red-400 text-center text-sm">{error}</p>
-        ) : null}
+        {error ? <ErrorText {...{ error }} /> : null}
         <div className="mx-auto">
           <Button loading={isLoading} disabled={isLoading} onClick={action}>
             Generate Token
